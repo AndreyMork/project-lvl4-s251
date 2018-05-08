d50e047d19bd4c79dbed6d3b6aaa3471fa14d70e
Andrey <magot.a.d@gmail.com>
2018-05-08 16:38:22 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: ['./src/index.js'],
+  output: {
+    // path: path.join(__dirname, 'public', 'assets'),
+    // filename: 'application.js',
+    publicPath: '/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [],
+};
