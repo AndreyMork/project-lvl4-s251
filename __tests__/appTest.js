181cfc17582f0a86354e2eb09816bc6c13ad8322
Andrey <magot.a.d@gmail.com>
2018-05-10 02:40:19 +0300

    add tests

---
+++ __tests__/appTest.js
@@ -0,0 +1,27 @@
+import request from 'supertest';
+import app from '..';
+
+describe('requests', () => {
+  let server;
+
+  beforeEach(() => {
+    server = app().listen();
+  });
+
+  it('GET 200', async () => {
+    const res = await request.agent(server)
+      .get('/');
+    expect(res.status).toBe(200);
+  });
+
+  it('GET 404', async () => {
+    const res = await request.agent(server)
+      .get('/wrong-path');
+    expect(res.status).toBe(404);
+  });
+
+  afterEach((done) => {
+    server.close();
+    done();
+  });
+});
