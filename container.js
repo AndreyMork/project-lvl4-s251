3f38a82ffc8c848647d422a67055a369a16f0bc7
Andrey <magot.a.d@gmail.com>
2018-05-18 17:35:21 +0300

    add validate

--- container.js
+++ container.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
+import validatejs from 'validate.js';
+import User from './entities/User';
 
+const validate = entity => validatejs(entity, entity.constructor.constraints);
 // import logger from './lib/logger';
 
 dotenv.config();
 
-export default { };
+export default { User, validate };
