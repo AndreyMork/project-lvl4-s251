805d3dde37d5914f4cf98942a64b5ea719933c0a
Andrey <magot.a.d@gmail.com>
2018-05-21 17:17:59 +0300

    add db in container

--- container.js
+++ container.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
 import validatejs from 'validate.js';
 import User from './entities/User';
+import db from './db';
 
 const validate = entity => validatejs(entity, entity.constructor.constraints);
 // import logger from './lib/logger';
 
 dotenv.config();
 
-export default { User, validate };
+export default { User, validate, db };
