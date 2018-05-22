545d4d9a43046b7f1a78a3d341e784bcd9f64e20
Andrey <magot.a.d@gmail.com>
2018-05-22 23:47:47 +0300

    chore

--- db/index.js
+++ db/index.js
@@ -5,6 +5,7 @@ import { Client } from 'pg';
 const client = new Client({ connectionString: process.env.DATABASE_URL, ssl: true });
 
 const insertUser = async (user) => {
+  await client.connect();
   const query = {
     text: 'INSERT INTO users (id, email, password, first_name, last_name) VALUES ($1, $2, $3, $4, $5)',
     values: [user.id, user.email, user.password, user.firstName, user.lastName],
@@ -15,13 +16,17 @@ const insertUser = async (user) => {
   } catch (err) {
     console.error(err.stack);
   }
+
+  client.end();
 };
 
 const getUsers = async () => {
+  await client.connect();
   const query = {
     text: 'SELECT email, first_name, last_name, id FROM users',
   };
   const res = await client.query(query);
+  client.end();
   return res.rows;
 };
 
