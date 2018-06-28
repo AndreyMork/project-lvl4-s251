a75b13cefcea3e4a2d2bce3f9eddb3458f52b191
Andrey <magot.a.d@gmail.com>
2018-06-28 23:30:45 +0300

    chore

--- models/index.js
+++ models/index.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 dotenv.load();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
-const User = sequelize.import('./User.js');
-// sequelize.sync();
+export const User = sequelize.import('./User.js');
 
 export default { User };
