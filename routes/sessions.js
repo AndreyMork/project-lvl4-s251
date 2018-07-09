006af5d24eb26fa0a1643ac1198a537771c73eb8
Andrey <magot.a.d@gmail.com>
2018-07-09 18:44:16 +0300

    chore

--- routes/sessions.js
+++ routes/sessions.js
@@ -6,7 +6,6 @@ export default (router) => {
     .get('newSession', '/sessions/new', (ctx) => {
       const { email } = ctx.session;
       delete ctx.session.email;
-<<<<<<< HEAD
 
       const viewArgs = {
         pageTitle: 'Sign In',
@@ -14,10 +13,6 @@ export default (router) => {
       };
 
       ctx.render('sessions/new', viewArgs);
-=======
-      ctx.state.formObj = buildFormObj({ email });
-      ctx.render('sessions/new', { pageTitle: 'Sign In' });
->>>>>>> 39e6f5eaef10936d5e6d3c3c06ac7a2951d9a062
     })
     .post('session', '/session', async (ctx) => {
       const { email, password } = ctx.request.body.form;
