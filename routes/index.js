25b194001cde3f6b6f03012582e7b41d40b097f5
Andrey <magot.a.d@gmail.com>
2018-07-17 06:44:17 +0300

    add statuses routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ import user from './user';
 import users from './users';
 import sessions from './sessions';
 import tasks from './tasks';
+import statuses from './statuses';
 import notFound from './notFound';
 
-const controllers = [pages, user, users, sessions, tasks, notFound];
+const controllers = [pages, user, users, sessions, tasks, statuses, notFound];
 
 export default (router, container) => (
   controllers.forEach(controller => controller(router, container))
